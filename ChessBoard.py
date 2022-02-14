6103439440a74faa8c2d518d950e7f728d71a066
= <=>
2022-02-14 11:26:57 -0800

    Got Basic Pawn movement working
    Worked on #1, got basic pawn movment forward working. En Passent still needs to be tested, however that will be tested after input parsing is made for pawns is working.

--- ChessBoard.py
+++ ChessBoard.py
@@ -3,6 +3,10 @@ from abc import ABC, abstractclassmethod
 
 FILE = 0
 RANK = 1
+UPPERCASE_OFFSET = 65
+
+def GetFileAsInt(file):
+    return ord(file)-UPPERCASE_OFFSET
 
 class ChessPiece():
     Position = ()
@@ -32,36 +36,36 @@ class Pawn(ChessPiece):
 
     def IsMoveValid(self, board, toPos):
         if self.Color == "White":
-            if toPos[RANK] == self.Position[RANK]+1:
-                if toPos[FILE] == self.Position[FILE]:
+            if toPos[RANK] == self.Position[RANK]-1:
+                if GetFileAsInt(self.Position[FILE]) == GetFileAsInt(self.Position[FILE]):
                     return True
                 else: #en passant
-                    if toPos[FILE] == self.Position[FILE] + 1:
-                        if type(board.GetPieceAtPos((self.Position[FILE]+1, self.Position[RANK]))) == Pawn:
-                            if board.GetPieceAtPos((self.Position[FILE]+1, self.Position[RANK])).doubleMoved == True:
+                    if GetFileAsInt(self.Position[FILE]) == GetFileAsInt(self.Position[FILE]) + 1:
+                        if type(board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])+1+UPPERCASE_OFFSET), self.Position[RANK]))) == Pawn:
+                            if board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])+1+UPPERCASE_OFFSET), self.Position[RANK])).doubleMoved == True:
                                 return True
                             else:
                                 return False
-                        if type(board.GetPieceAtPos((self.Position[FILE]-1, self.Position[RANK]))) == Pawn:
-                            if board.GetPieceAtPos((self.Position[FILE]-1, self.Position[RANK])).doubleMoved == True:
+                        if type(board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])-1+UPPERCASE_OFFSET), self.Position[RANK]))) == Pawn:
+                            if board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])-1+UPPERCASE_OFFSET), self.Position[RANK])).doubleMoved == True:
                                 return True
                             else:
                                 return False
             else:
                 return False
         if self.Color == "Black":
-            if toPos[RANK] == self.Position[RANK]-1:
-                if toPos[FILE] == self.Position[FILE]:
+            if toPos[RANK] == self.Position[RANK]+1:
+                if GetFileAsInt(self.Position[FILE]) == GetFileAsInt(self.Position[FILE]):
                     return True
                 else: #en passant
-                    if toPos[FILE] == self.Position[FILE] + 1:
-                        if type(board.GetPieceAtPos((self.Position[FILE]+1, self.Position[RANK]))) == Pawn:
-                            if board.GetPieceAtPos((self.Position[FILE]+1, self.Position[RANK])).doubleMoved == True:
+                    if GetFileAsInt(self.Position[FILE]) == GetFileAsInt(self.Position[FILE]) + 1:
+                        if type(board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])+1+UPPERCASE_OFFSET), self.Position[RANK]))) == Pawn:
+                            if board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])+1+UPPERCASE_OFFSET), self.Position[RANK])).doubleMoved == True:
                                 return True
                             else:
                                 return False
-                        if type(board.GetPieceAtPos((self.Position[FILE]-1, self.Position[RANK]))) == Pawn:
-                            if board.GetPieceAtPos((self.Position[FILE]-1, self.Position[RANK])).doubleMoved == True:
+                        if type(board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])-1+UPPERCASE_OFFSET), self.Position[RANK]))) == Pawn:
+                            if board.GetPieceAtPos((chr(GetFileAsInt(self.Position[FILE])-1+UPPERCASE_OFFSET), self.Position[RANK])).doubleMoved == True:
                                 return True
                             else:
                                 return False
@@ -85,7 +89,7 @@ class Chessboard():
             for file in range(self.BOARD_SIZE+1):
                 if rank == 0:
                     if file != self.BOARD_SIZE:
-                        print(f"{chr(file+65)}", end = " ")
+                        print(f"{chr(file+UPPERCASE_OFFSET)}", end = " ")
                     continue
                 if file == 0:
                     print(f"{rank}", end = " ")
@@ -97,36 +101,36 @@ class Chessboard():
                     print(self.board[rank-1][file-1].PieceChar, end = " ")
                     continue
             print()
-                
 
     def GetPieceAtPos(self, position):
-        return self.board[position[FILE]][position[RANK]]
+        return self.board[position[RANK]][GetFileAsInt(position[FILE])]
 
     def DeletePieceAtPos(self, position):
-        self.board[position[FILE]][position[RANK]] = None
+        self.board[position[RANK]][GetFileAsInt(position[FILE])] = None
 
     def PlacePiece(self, piece, position):
-        self.board[position[FILE]][position[RANK]] = piece
+        self.board[position[RANK]][GetFileAsInt(position[FILE])] = piece
 
     def MovePiece(self, origPos, newPos):
-        if self.GetPieceAtPos(origPos).IsMoveValid(newPos):
+        if self.GetPieceAtPos(origPos).IsMoveValid(self.board, newPos):
             self.PlacePiece(self.GetPieceAtPos(origPos), newPos)
             self.DeletePieceAtPos(origPos)
 
     def SetBoard(self):
         for x in range(8):
-            pawn = Pawn(("A", 1), "White", "P")
-            self.PlacePiece(pawn, (1, x))
+            pawn = Pawn((chr(x+UPPERCASE_OFFSET), 1), "White", "P")
+            self.PlacePiece(pawn, (chr(x+UPPERCASE_OFFSET), 1))
 
         for x in range(8):
             pawn = Pawn(("A", 1), "Black", "P")
-            self.PlacePiece(pawn, (6, x))
+            self.PlacePiece(pawn, (chr(x+UPPERCASE_OFFSET), 6))
 
 def main():
     board = Chessboard()
     board.SetBoard()
     board.PrintBoard()
-    board.MovePiece((1, 1), (1, 2))
+    board.MovePiece(("A", 1), ("A", 2))
+    board.PrintBoard()
 
 if __name__ == "__main__":
     main()
